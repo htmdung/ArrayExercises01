e42bf0ff2cad12606c480449c824fc339fdffee2
Dung Huynh <htmdung1011@gmail.com>
2020-10-20 00:34:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Create an array with the following entry: 273.15.
+let myArray = [273.15];
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+myArray.push(42, "hello");
+console.log(myArray);
+
+
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
+myArray.push(false, -4.6, "87");
+console.log(myArray);
